0e43d39a092e3aad0b9125cf2a8e347ac6d084a1
Gilles Rousseaux <gilles1996@outlook.com>
2023-01-28 21:48:40 +0100

    Delete EindProject directory

--- EindProject/project/mel/src/components/PersonAvatar.js
+++
@@ -1,8 +0,0 @@
-import Avatar from "boring-avatars";
-
-export function PersonAvatar(props) {
-    const { person } = props;
-    if (!person) return;
-
-    return <Avatar size="50" name={person.id} variant="beam" />;
-}
--- EindProject/project/mel/src/pages/MenuCardPage.js
+++
@@ -1,9 +0,0 @@
-import {MenuCard} from "../components/MenuCard";
-
-export function MenuCardPage(props) {
-    const { products } = props;
-
-    return (
-        <MenuCard products={products} />
-    );
-}
