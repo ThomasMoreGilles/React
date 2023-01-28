53ef76267f2992a792c118bdc70bb1e3a81bcefc
gilles20393 <gilles1996@outlook.com>
2023-01-28 23:34:46 +0100

    colors update / card.jsx uitleg

--- eindopdracht/src/UI/Card.jsx
+++ eindopdracht/src/UI/Card.jsx
@@ -1,3 +1,9 @@
+/* 	De card.jsx component is verantwoordelijk voor het weergeven van de inhoud van een enkel item in de applicatie.
+ 	Dit kan bijvoorbeeld een product, een trainer of een ander soort item zijn.
+ 	De component verwacht een aantal props, zoals een afbeelding, een titel en een beschrijving, die gebruikt worden om de inhoud van de kaart weer te geven.
+ 	De component kan ook optioneel props voor knoppen of links ontvangen die worden weergegeven onderaan de kaart.
+ 	De uiteindelijke weergave van de kaart is gedaan met HTML en CSS. 	*/
+
 import React from "react";
 
 const Card = ({ className, children }) => {
