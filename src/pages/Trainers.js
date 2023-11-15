e15783f635f2c0911faed10be3228e174cfa67bf
gllsrssx <glls@rssx.eu>
2023-11-15 18:38:11 +0100

    Add Trainers page with Trainer component

---
+++ src/pages/Trainers.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Header from "../components/Header";
+import HeaderImage from "../images/header_bg_5.jpg";
+import { BsInstagram } from "react-icons/bs";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { trainers } from "../data";
+import Trainer from "../components/Trainer";
+
+const Trainers = () => {
+	return (
+		<>
+			<Header image={HeaderImage} title="Our Trainers">
+				Meet our experienced and highly qualified trainers who are dedicated to helping you achieve your fitness goals. From personal training to group classes, our team is here to support you every step of the way.
+			</Header>
+			<section className="trainers">
+				<div className="container trainers__container">
+					{trainers.map(({ id, image, name, job, socials }) => {
+						return (
+							<Trainer
+								key={id}
+								image={image}
+								name={name}
+								job={job}
+								socials={[
+									{ icon: <BsInstagram />, link: socials[0] },
+									{ icon: <AiOutlineTwitter />, link: socials[1] },
+									{ icon: <FaFacebookF />, link: socials[2] },
+									{ icon: <FaLinkedinIn />, link: socials[3] },
+								]}
+							/>
+						);
+					})}
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default Trainers;
