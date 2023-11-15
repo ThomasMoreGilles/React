462f9115664dd05a39ba83890b41cf237447b8a8
gllsrssx <glls@rssx.eu>
2023-11-15 18:37:59 +0100

    Add Gallery page with images and header

---
+++ src/pages/Gallery.js
@@ -0,0 +1,32 @@
+import React from "react";
+import HeaderImage from "../images/header_bg_3.jpg";
+import Header from "../components/Header";
+
+function Gallery() {
+	const galleryLength = 15;
+	const images = [];
+	for (let i = 1; i <= galleryLength; i++) {
+		images.push(require(`../images/gallery${i}.jpg`));
+	}
+
+	return (
+		<>
+			<Header title="Our Gallery" image={HeaderImage}>
+				Explore a collection of our finest work in our gallery, featuring a diverse range of projects crafted by our skilled team of architects. Discover the attention to detail and passion that goes into every one of our creations.
+			</Header>
+			<section className="gallery">
+				<div className="container gallery__container">
+					{images.map((image, index) => {
+						return (
+							<article key={index}>
+								<img src={image} alt={`GalleryImage ${index + 1}`} />
+							</article>
+						);
+					})}
+				</div>
+			</section>
+		</>
+	);
+}
+
+export default Gallery;
