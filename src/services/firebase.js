fb2f303b79a34aeaa2abc5de6e087f05961e18eb
gllsrssx <glls@rssx.eu>
2023-11-15 18:38:14 +0100

    Update Firebase configuration

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,17 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyA7yDCpbvf9D9-7fYUTMgnNj9lV0feXKAg",
+  authDomain: "project-4d980.firebaseapp.com",
+  projectId: "project-4d980",
+  storageBucket: "project-4d980.appspot.com",
+  messagingSenderId: "874895242591",
+  appId: "1:874895242591:web:2044f2b5c358b2f27d32ea"
 };
+
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-export const firestoreDB = getFirestore(firebaseApp);
-console.log("initialized firebase connection");
+const app = initializeApp(firebaseConfig);
