16ac9b40a2a5fe6159c5b457c656ba03785ec392
gllsrssx <glls@rssx.eu>
2023-11-15 18:38:02 +0100

    Add Home page components.

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MainHeader from "../components/MainHeader";
+import Program from "../components/Program";
+import Values from "../components/Values";
+import FAQs from "../components/FAQs";
+import Testimonial from "../components/Testimonial";
+
+const Home = () => {
+	return (
+		<>
+			<MainHeader />
+			<Program />
+			<Values />
+			<FAQs />
+			<Testimonial />
+		</>
+	);
+};
+
+export default Home;
